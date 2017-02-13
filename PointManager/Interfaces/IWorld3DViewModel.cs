059abcbc1efce36534b1c085580f458a5c31c951
Zexuz <Robin.edbom@gmail.com>
2017-02-13 13:27:22 +0100

    Created a World3D View Model and a interface for it. Also implemeted the interface methods

---
+++ PointManager/Interfaces/IWorld3DViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PointManager.Interfaces
+{
+    public interface IWorld3DViewModel
+    {
+        void OnKeyDown(object sender, KeyEventArgs e);
+        void OnKeyUp(object sender, KeyEventArgs e);
+        void OnLoaded(object sender, RoutedEventArgs e);
+        void OnMouseOver(object sender, MouseEventArgs e);
+    }
+}
---
+++ PointManager/ViewModels/World3DViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using PointManager.Interfaces;
+
+namespace PointManager.ViewModels
+{
+    public class World3DViewModel : ViewModelBase, IWorld3DViewModel
+    {
+        public void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public void OnMouseOver(object sender, MouseEventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
